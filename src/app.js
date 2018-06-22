b6527d3c3d7b35c30108f880ba003c8e9b278483
Calvin Wilkinson <kinsondigital@gmail.com>
2018-06-22 13:37:39 -0400

    Move files around and improve npm scripts

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-import { add, subtract, multiply, divide } from "../src/my-math";
-import { SomeLib } from "../src/some-lib";
+import { add, subtract, multiply, divide } from "../libs/my-math";
+import { SomeLib } from "../libs/some-lib";
 
 let lib = new SomeLib();
 
