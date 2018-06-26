9a747f60748e063456a037c784d6084b1cfbcb08
Calvin Wilkinson <kinsondigital@gmail.com>
2018-06-26 09:29:32 -0400

    Improve webpack config pathing and npm scripts

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,13 +1,8 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 let path = require("path");
 
-/*NOTE:
-	Paths in webpack configs are relative from the package.json file,
-	not the webpack config file
-*/
-
 module.exports = {
-	entry: "./src/app.js",
+	entry: path.resolve(__dirname, "../src/app.js"),
 	output: {
 		path: path.resolve(__dirname, "../build"),
 		filename: "app.bundle.js"
